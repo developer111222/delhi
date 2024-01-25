8684bb7d3e5dd47c3db9a8b981a5bd93675cc457
developer111222 <anishprajapati765@gmail.com>
2024-01-24 20:50:52 -0800

    first commit

--- frontend/src/components/layout/header/assets/Search.js
+++ frontend/src/components/layout/header/assets/Search.js
@@ -6,6 +6,7 @@ import { ClearError, searchProduct } from "../../../../actions/ProductAction";
 import Loader from "../../loader/Loader";
 import { NavLink } from "react-router-dom";
 import "./search.css";
+import { StarComponent } from "../../../productDetails/assets/StarComponent";
 
 export const Search = () => {
   const dispatch = useDispatch();
@@ -68,7 +69,20 @@ export const Search = () => {
                           src={`http://localhost:3000/${item.imageId[0].path}`}
                           alt={item.name}
                         />
-                        <p>{item.name}</p>
+                        <div style={{display:"block",width:"100%"}}>
+                        <div>
+                        <p>{item.name}</p></div>
+                      
+                        <div className="search-star" style={{display:"flex",justifyContent:"space-between"}}>
+                         
+                          <p>
+                            price: <ins>{item.price}</ins>
+                          </p>
+                          <p>
+                            maxprice: <del>{item.maxprice}</del>
+                          </p>
+                          <StarComponent review={item.ratings} />
+                        </div></div>
                       </NavLink>
                     </li>
                   ))
